b7fa06c4c15bb094de0af881c8e041ae96fa7fcb
PickYerPoison <grncp5@mst.edu>
2014-11-18 19:50:26 -0600

    Added empty classes for the two new AIs
    Also updated javadocs.

---
+++ src/BalancedPlayer.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+/**
+ * @author Graham
+ *
+ */
+public class BalancedPlayer extends Player {
+
+	/**
+	 * 
+	 */
+	public BalancedPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see Player#defend(Node, Node, int)
+	 */
+	@Override
+	int defend(Node from, Node to, int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see Player#occupy(Node, Node)
+	 */
+	@Override
+	int occupy(Node from, Node to) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see Player#place()
+	 */
+	@Override
+	Node place() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see Player#turn()
+	 */
+	@Override
+	void turn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
