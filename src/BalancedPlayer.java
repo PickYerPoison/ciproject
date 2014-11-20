03bdc550c91f9726fc485e081cc13920ad85dc15
PickYerPoison <grncp5@mst.edu>
2014-11-20 15:49:07 -0600

    Work on BalancedPlayer

--- src/BalancedPlayer.java
+++ src/BalancedPlayer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -12,7 +14,7 @@ public class BalancedPlayer extends Player {
 	 * 
 	 */
 	public BalancedPlayer() {
-		// TODO Auto-generated constructor stub
+		super("BalancedPlayer");
 	}
 
 	/**
@@ -20,7 +22,6 @@ public class BalancedPlayer extends Player {
 	 */
 	public BalancedPlayer(String n) {
 		super(n);
-		// TODO Auto-generated constructor stub
 	}
 
 	/* (non-Javadoc)
@@ -28,8 +29,11 @@ public class BalancedPlayer extends Player {
 	 */
 	@Override
 	int defend(Node from, Node to, int num) {
-		// TODO Auto-generated method stub
-		return 0;
+		// always defend with as many units as possible
+		if (to.getUnits() > 1)
+			return 2;
+		else
+			return 1;
 	}
 
 	/* (non-Javadoc)
@@ -37,8 +41,8 @@ public class BalancedPlayer extends Player {
 	 */
 	@Override
 	int occupy(Node from, Node to) {
-		// TODO Auto-generated method stub
-		return 0;
+		// keep half the units (round down)
+		return from.getUnits() - (from.getUnits()/2);
 	}
 
 	/* (non-Javadoc)
@@ -46,7 +50,64 @@ public class BalancedPlayer extends Player {
 	 */
 	@Override
 	Node place() {
-		// TODO Auto-generated method stub
+		// determine if this is the first time we're placing a unit
+		if (graph.getNumOwnedNodes(this) == 0) {
+			
+		}
+		// if we already have units on the field
+		else {
+			// are there unowned nodes on the field? (initial placement phase)
+			if (graph.getNumOwnedNodes(null) > 0) {
+				// see if we can place a unit in a node next to one of our own
+				
+				// get an ArrayList of nodes we own
+				ArrayList<Node> nodes = graph.getOwnedNodes(this);
+				
+				// create an ArrayList to store candidates for placement
+				ArrayList<Node> toPlace = new ArrayList<Node>(0);
+				
+				// iterate through all of our nodes and add any adjacent unowned nodes
+				for (Node node : nodes) {
+					for (Node adjNode : node.getAdj()) {
+						if (adjNode.getOwner() == null)
+							toPlace.add(adjNode);
+					}
+				}
+				
+				// did we find any candidates?
+				if (toPlace.size() >= 1) {
+					// if so, place a unit at the first node we found
+					return toPlace.get(0);
+				}
+				// did we find no candidates?
+				else {
+					// place a unit at an unowned node
+					return graph.getOwnedNodes(null).get(0);
+				}
+			}
+			// do all the nodes have units in them? (regular placement phase)
+			else {
+				// place the new unit in the most threatened node we own
+				
+				// create an ArrayList of all our nodes
+				ArrayList<Node> nodes = graph.getOwnedNodes(this);
+				
+				// iterate through to find the most threatened node
+				int max = 0;
+				Node toPlace = nodes.get(0); 
+				for (Node node : nodes) {
+					int threat = graph.getThreat(range, node);
+					if (threat > max) {
+						max = threat;
+						toPlace = node;
+					}
+				}
+				
+				// place the unit at the most threatened node
+				return toPlace;
+			}
+		}
+		
 		return null;
 	}
 
